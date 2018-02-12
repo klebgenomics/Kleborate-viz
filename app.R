83aeb9978d87e08d8a71e36f47060da6fe9f4fd1
Cassie Litchfield <casslitch@gmail.com>
2018-02-12 14:21:41 +1100

    Adding version 1 script

--- app.R
+++ app.R
@@ -4,124 +4,54 @@ library(heatmaply)
 library(dplyr)
 library(reshape2)
 
-# Define UI for app that draws a histogram ----
+#Input data
+kleborate_data <- read.csv("kleborate_viz_test_data_mixedSTs.txt",sep="\t")
+
+
+# Define UI for Shiny App: Kleborate Visualiser
 ui <- fluidPage(
   
-  # App title ----
+  # App title
   titlePanel(title=div(img(src="logo.png",height=100,width=200),align="center")),
   
+  # Tab layout
   tabsetPanel(
-    tabPanel("Resistance Score", plotOutput("distPlot")), 
-    tabPanel("Virulence Score", plotOutput("distPlot2")), 
+    tabPanel("Resistance Score", plotOutput("ResistancePlot")), 
+    tabPanel("Virulence Score", plotOutput("VirulencePlot")), 
     tabPanel("ST Distribution",      
       fluidRow(
-        column(4,wellPanel(sliderInput(inputId = "bins", label = "Number of bars:",min = 1,max = nlevels(kleborate_data$ST),step =1,
+        column(4,wellPanel(sliderInput(inputId = "bars", label = "Number of bars:",min = 1,max = nlevels(kleborate_data$ST),step =1,
                 value = nlevels(kleborate_data$ST)))),
-        column(8,plotOutput("distPlot3"))
-        
+        column(8,plotOutput("SThist"))
     )),
-    tabPanel("Heat Map",
-             plotOutput("heatmap"))
-    
+    tabPanel("Heat Map", plotOutput("heatmap"))
     )
   )
-  
-  # fluidRow(
-  #   plotOutput("distPlot")
-  # ),
-  # 
-  # fluidRow(
-  #   plotOutput("distPlot2")
-  # ),
-  # 
-  # fluidRow(
-  #   
-  #   column(4,
-  #          wellPanel(
-  #            sliderInput(inputId = "bins",
-  #                                        label = "Number of bars:",
-  #                                        min = 1,
-  #                                        max = nlevels(kleborate_data$ST), step =1,
-  #                                        value = nlevels(kleborate_data$ST)
-  #                                      )
-  #          )       
-  #   ),
-  #   
-  #   column(8,
-  #          plotOutput("distPlot3")
-  #   )
-  # ),
-  # 
-  # 
-  # fluidRow(
-  #   plotOutput("heatmap")
-  # )
-  # 
-  
-  # # Sidebar layout with input and output definitions ----
-  # sidebarLayout(
-  #   
-  #   
-  #   # Sidebar panel for inputs ----
-  #    sidebarPanel(
-  #     
-  #     #Input: Slider for the number of bins ----
-  #     sliderInput(inputId = "bins",
-  #                 label = "Number of bars:",
-  #                 min = 1,
-  #                 max = nlevels(kleborate_data$ST), step =1, 
-  #                 value = nlevels(kleborate_data$ST)
-  #               )
-  # 
-  #   ),
-  #   # Main panel for displaying outputs ----
-  #   mainPanel(
-  #     
-  #     # Output: Histogram ----
-  #     plotOutput(outputId = "distPlot"),
-  #     plotOutput(outputId = "distPlot2"),
-  #     plotOutput(outputId = "distPlot3"),
-  #     plotlyOutput("heatmap")
-  #     
-  #   )
-  # )
 
 
-# Define server logic required to draw a histogram ----
+# Define server logic for app
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-   # x    <- faithful$waiting
-  #  bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-  #interactive bar graph
-  #ggplot(data=faithful, aes(faithful$waiting)) + 
-   # geom_histogram(breaks=bins)
+  #Generate colours for plots (based on logo colours)
+  species_cols <- colorRampPalette(c("#e67d77", "#f1c280", "#f5ecd1", "#98c4ca", "#7f8288"))(nlevels(kleborate_data$species))
   
-  #resistance score
-  ggplot(kleborate_data, aes(x = as.factor(resistance_score), fill = species)) + geom_bar() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + scale_y_continuous(expand=c(0,0)) + scale_fill_brewer(palette = "Accent") + ylab("Number of isolates") + xlab("Resistance score") + labs(fill = "Species")
+  #Resistance score plot
+  output$ResistancePlot <- renderPlot({
+  ggplot(kleborate_data, aes(x = as.factor(resistance_score), fill = species)) + geom_bar() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + scale_y_continuous(expand=c(0,0)) + scale_fill_manual(values = species_cols) + ylab("Number of isolates") + xlab("Resistance score") + labs(fill = "Species")
   
   })
   
-  output$distPlot2 <- renderPlot({
-    #virulence score
-    ggplot(kleborate_data, aes(x = virulence_score, fill = species)) + geom_bar() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + scale_y_continuous(expand=c(0,0)) + scale_fill_brewer(palette = "Accent") + ylab("Number of isolates") + xlab("Virulence score") + labs(fill = "Species")  
+  #Virulence score plot
+  output$VirulencePlot <- renderPlot({
+  ggplot(kleborate_data, aes(x = virulence_score, fill = species)) + geom_bar() + theme(axis.text.x = element_text(colour = "black", size = 12), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + scale_y_continuous(expand=c(0,0)) + scale_fill_manual(values = species_cols) + ylab("Number of isolates") + xlab("Virulence score") + labs(fill = "Species")  
   })
   
-  #ST histogram
-  output$distPlot3 <- renderPlot({
-    ggplot(kleborate_data, aes(x=reorder(ST,ST,function(x)-length(x)))) + geom_bar() + theme(axis.text.x = element_text(colour = "black", size = 12,angle = 45, hjust = 1), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + ylab("Number of isolates") + xlab("ST") + scale_y_continuous(expand=c(0,0))+ scale_x_discrete(limits = (levels(reorder(kleborate_data$ST,kleborate_data$ST,function(x)-length(x)))[1:input$bins]))
+  #Sequence type histogram (interactive)
+  output$SThist <- renderPlot({
+    ggplot(kleborate_data, aes(x=reorder(ST,ST,function(x)-length(x)))) + geom_bar(fill = "#F1C280") + theme(axis.text.x = element_text(colour = "black", size = 12,angle = 45, hjust = 1), axis.text.y = element_text(colour = "black", size = 12), axis.title = element_text(colour = "black", size = 14), panel.background = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) + ylab("Number of isolates") + xlab("ST") + scale_y_continuous(expand=c(0,0))+ scale_x_discrete(limits = (levels(reorder(kleborate_data$ST,kleborate_data$ST,function(x)-length(x)))[1:input$bars]))
   })
   
+  #Heat map (interactive)
   output$heatmap <- renderPlot({
     vir_res <- table(factor(kleborate_data$virulence_score,c(0,1,2,3)),factor(kleborate_data$resistance_score,c(0,1,2)))
     vir_res <- as.data.frame.matrix(vir_res)
@@ -132,4 +62,5 @@ server <- function(input, output) {
   
 }
 
+#Load shiny app
 shinyApp(ui = ui, server = server)
