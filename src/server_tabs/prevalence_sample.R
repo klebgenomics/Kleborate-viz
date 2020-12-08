6e13722a46843449f91c6b15f1194f5f633d570b
scwatts <s.watts2@student.unimelb.edu.au>
2020-12-08 19:54:33 +1100

    Add working prevalence by sample plots

--- src/server_tabs/prevalence_sample.R
+++ src/server_tabs/prevalence_sample.R
@@ -1,7 +1,18 @@
+# Get summary data
+metadata_summary_sample <- reactive({
+  left_join(data_loaded$metadata, data_loaded$kleborate[data_selected$rows, ]) %>%
+    group_by(Source, Classification) %>%
+    summarise(
+      n=n(), 
+      mean_virulence_score = mean(virulence_score), 
+      mean_resistance_score = mean(resistance_score)
+    )
+})
+# Plot
 output$prevalence_sample_scatter <- renderPlotly({
   # TODO: selection by type (i.e. variables other than sample)
-  g <- ggplot(sample_vir_res, aes(x=mean_vir, y=mean_res)) +
-    geom_point(aes(size=total, color=sample)) +
+  g <- ggplot(metadata_summary_sample(), aes(x=mean_virulence_score, y=mean_resistance_score)) +
+    geom_point(aes(size=n, color=Source)) +
     theme_bw()
   g <- ggplotly(g, dynamicTicks=TRUE)
   print(g)
