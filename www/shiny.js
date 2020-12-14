a73a7d1103f322dfc61cd2ba22823d40123c088e
scwatts <s.watts2@student.unimelb.edu.au>
2020-12-14 18:59:33 +1100

    Make tab hiding reversible by showTab R calls

--- www/shiny.js
+++ www/shiny.js
@@ -1,12 +1,12 @@
 // More quickly hide unavailable tabs at load
 shinyjs.init = function() { 
-  $('#primary li a[data-value="Summary by species"]').hide();
-  $('#primary li a[data-value="Genotypes by ST"]').hide();
-  $('#primary li a[data-value="Genotypes by metadata"]').hide();
-  $('#primary li a[data-value="Convergence by ST"]').hide();
-  $('#primary li a[data-value="K/O diversity by ST"]').hide();
-  $('#primary li a[data-value="Temporal trends"]').hide();
-  $('#primary li a[data-value="Sample trends"]').hide();
-  $('#primary li a[data-value="Cumulative K/O prevalence"]').hide();
-  $('#primary li a[data-value="MICs by AMR genotype"]').hide();
+  $('#primary li a[data-value="Summary by species"]').parent().hide();
+  $('#primary li a[data-value="Genotypes by ST"]').parent().hide();
+  $('#primary li a[data-value="Genotypes by metadata"]').parent().hide();
+  $('#primary li a[data-value="Convergence by ST"]').parent().hide();
+  $('#primary li a[data-value="K/O diversity by ST"]').parent().hide();
+  $('#primary li a[data-value="Temporal trends"]').parent().hide();
+  $('#primary li a[data-value="Sample trends"]').parent().hide();
+  $('#primary li a[data-value="Cumulative K/O prevalence"]').parent().hide();
+  $('#primary li a[data-value="MICs by AMR genotype"]').parent().hide();
 }
