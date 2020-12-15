d1e0c5640f4e744938081f36969c9f50174e3c38
scwatts <s.watts2@student.unimelb.edu.au>
2020-12-15 16:23:30 +1100

    Remove unused code

--- src/server_tabs/genotype_metadata.R
+++ src/server_tabs/genotype_metadata.R
@@ -59,32 +59,11 @@ genotype_metadata_dist_plot <- reactive({
     # Set annotation column
     d$annotation <- ifelse(d[[input$genotype_metadata_dist_plot_anno]]=='-', 'absent', 'present')
   }
-  
-#  # ST number slider
-#output$genotype_group_count <- renderUI({
-#  sliderInput(
-#    inputId='genotype_group_count',
-#    label='Number of groups:',
-#    min=1,
-#    max=length(unique(input$genotype_metadata_dist_plot_group)),
-#    value=min(5, length(unique(input$genotype_metadata_dist_plot_group)))
-#  )
-#})
-
-  
-  
   # Order by group size
-  d <<- d
-  
   d$group <- d[[input$genotype_metadata_dist_plot_group]]
   v.group_counts <- sort(table(d$group), decreasing=TRUE)
   v.group_order <- names(v.group_counts)
   d$group <- factor(d$group, levels=v.group_order)
-  
-    
-  # Select first n STs
- # d <- d[d$group %in% v.group_order[1:input$genotype_group_count], ]
-  
   # Create plot
   g <- ggplot(data=d, aes(x=group, fill=annotation))
   g <- g + geom_bar()
